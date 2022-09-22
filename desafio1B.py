8ccafcfc00fe02295b9e2994db2bc4c7a3d3e97c
LucasEzequielGiordano <lucasgiordano2219@gmail.com>
2022-09-22 20:22:31 -0300

    feat: champion and last obtained

---
+++ desafio1B.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_excel("https://raw.githubusercontent.com/IEEESBITBA/Curso-Python/master/Curso_Analisis_de_Datos_Datos/Tabla1.xlsx")
+sort =  df.sort_values('Puntos', ascending=False)
+records = sort.to_dict('records')
+
+print(f"Campeón: {records[0]['Equipo']} - Puntos: {int(records[0]['Puntos'])}")
+print(f"Último: {records[-1]['Equipo']} - Puntos: {int(records[-1]['Puntos'])}")
