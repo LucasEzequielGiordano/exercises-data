bd753f412b8d370ebf95dc851adce2d6a72abece
LucasEzequielGiordano <lucasgiordano2219@gmail.com>
2022-09-22 20:52:48 -0300

    feat: add teams with scores over 20

---
+++ desafio1C.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.read_excel("https://raw.githubusercontent.com/IEEESBITBA/Curso-Python/master/Curso_Analisis_de_Datos_Datos/Tabla1.xlsx")
+sort = df.sort_values('Puntos', ascending=False)
+points = sort.to_dict('records')
+
+print('Los equipos que obtuvieron puntaje mayor a 20 pts de forma descendiente')
+for n in points:
+    if n['Puntos'] > 20:
+        print(f"Equipo: {n['Equipo']}. Puntos: {int(n['Puntos'])}, Goles a favor: {int(n['Goles a favor'])}, Goles en contra: {int(n['Goles en contra'])}")
