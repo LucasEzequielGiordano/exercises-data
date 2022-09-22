1acf2055a6a152593e0a4612b85f0dc936ac260b
LucasEzequielGiordano <lucasgiordano2219@gmail.com>
2022-09-22 19:44:02 -0300

    fix: fix numbers int

--- desafio1A.py
+++ desafio1A.py
@@ -3,4 +3,4 @@ import pandas as pd
 df = pd.read_excel("https://raw.githubusercontent.com/IEEESBITBA/Curso-Python/master/Curso_Analisis_de_Datos_Datos/Tabla1.xlsx")
 records = df.to_dict('records')
 for i in records:
-    print(i['Equipo'], ' - ' ,i['Goles a favor'] - i['Goles en contra'])
+    print(i['Equipo'], ' - ' ,int(i['Goles a favor']) - int(i['Goles en contra']))
